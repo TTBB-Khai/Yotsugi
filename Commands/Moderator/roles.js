0b7a9437188c5abcf8b914793577580229bab00e
khai-bui <b_quockhai@fanshaweonline.ca>
2021-04-08 21:30:32 -0400

    added join message command to env

--- Commands/Moderator/roles.js
+++ Commands/Moderator/roles.js
@@ -4,14 +4,14 @@ const { responder: responder } = require(path.join(process.cwd(), 'Utils', 'Resp
 
 TTBT.on("guildMemberAdd", (guild, member) => {
 	guild.members.get(member.id).addRole(process.env['WELCOME_ROLE_ID'], "This role was given when they joined the server");
-	TTBT.createMessage(process.env['WELCOME_CHANNEL_ID'], `Welcome ${member.mention}, type 'hello' to join.\nWARNING: You can only do this once!`);
+	TTBT.createMessage(process.env['WELCOME_CHANNEL_ID'], `Welcome ${member.mention}, type '${process.env['JOIN_MESSAGE']}' to join.\nWARNING: You can only do this once!`);
 });
 
 TTBT.on('messageCreate', msg => {
-	if (msg.content === "hello" && msg.channel.id === process.env['WELCOME_CHANNEL_ID'] && msg.author.id !== process.env['CLIENT_ID']) {
+	if (msg.content === process.env['JOIN_MESSAGE'] && msg.channel.id === process.env['WELCOME_CHANNEL_ID'] && msg.author.id !== process.env['CLIENT_ID']) {
 		msg.channel.guild.members.get(msg.author.id).addRole(process.env['JOIN_ROLE_ID'], "This role was given when the user successfully gave themselves a role");
 		msg.channel.guild.members.get(msg.author.id).removeRole(process.env['WELCOME_ROLE_ID'], "This role was removed when the user successfully gave themselves a role");
-	} else if (msg.content !== "hello" && msg.channel.id === process.env['WELCOME_CHANNEL_ID'] && msg.author.id !== process.env['CLIENT_ID']) {
+	} else if (msg.content !== process.env['JOIN_MESSAGE'] && msg.channel.id === process.env['WELCOME_CHANNEL_ID'] && msg.author.id !== process.env['CLIENT_ID']) {
 		TTBT.createMessage(msg.channel.id, "...you fucking idiot");
 	}
 });
